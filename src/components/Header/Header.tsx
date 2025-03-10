2848b0645af4fd86360071b8e15d78c8a89888ca
RaulilloSierra <raulitosierradiaz@gmail.com>
2025-03-10 10:44:04 -0500

    Header responsivo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,61 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import "./Header.css";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="header">
-      <div></div>
-      <nav className="opciones">
-        <NavLink className={({ isActive }) => isActive ? "active opcion" : "opcion"} to="/">
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={`opciones ${isOpen && "open"}`}>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active opcion" : "opcion")}
+          to="/"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           Inicio
         </NavLink>
-        <NavLink className="opcion" to="/sobre-mi">
+        <NavLink
+          className="opcion"
+          to="/sobre-mi"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           Sobre Mí
         </NavLink>
-        <NavLink className="opcion" to="/proyectos">
+        <NavLink
+          className="opcion"
+          to="/proyectos"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           Proyectos
         </NavLink>
-        <NavLink className="opcion" to="/formacion">
+        <NavLink
+          className="opcion"
+          to="/formacion"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           Formación
         </NavLink>
-        <NavLink className="opcion" to="/skills">
+        <NavLink
+          className="opcion"
+          to="/skills"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           Habilidades
         </NavLink>
-        <NavLink className="opcion" to="/contacto">
+        <NavLink
+          className="opcion"
+          to="/contacto"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           Contacto
         </NavLink>
       </nav>
